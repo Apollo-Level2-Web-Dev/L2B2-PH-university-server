32d0bf2c4dac9a387efcc9cce81aab5c0ee9b4fd
abedinforhan <abedinforhan@gmail.com>
2023-11-16 14:57:38 +0600

    [DONE] student interfacen and schema

---
+++ src/app/modules/student.model.ts
@@ -0,0 +1,88 @@
+import { Schema , model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student/student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuradianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: userNameSchema,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddres: { type: String, required: true },
+  guardian: guardianSchema,
+  localGuardian: localGuradianSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+
+const Student = model<Student>('Student',studentSchema);
---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloogGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddres: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
