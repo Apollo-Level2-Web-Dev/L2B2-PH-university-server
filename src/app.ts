cf18e67537ba62c03795436e688a9376425df9dc
abedinforhan <abedinforhan@gmail.com>
2023-11-16 11:09:39 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+
+  res.send(a);
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
