5bcfa6f2c630e1a5f342f66c6b6cfa41fc445884
abedinforhan <abedinforhan@gmail.com>
2023-11-23 00:41:14 +0600

    updated

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,9 @@
+
+export type TUser = {
+  id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'admin' | 'student' | 'faculty';
+  status:'in-progress'|'blocked';
+  isDeleted: boolean;
+};
