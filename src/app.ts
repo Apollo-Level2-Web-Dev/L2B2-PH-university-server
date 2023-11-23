1cb6008679a47b0744b5cb45e21068802bd1935c
abedinforhan <abedinforhan@gmail.com>
2023-11-23 23:07:51 +0600

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -16,15 +15,14 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-const getAController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 app.use(globalErrorHandler);
 
