05f5da3937ed4dd33b289038d83635f886968adb
abedinforhan <abedinforhan@gmail.com>
2023-12-11 19:57:04 +0600

    test

--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -5,45 +5,55 @@ import {
   TPreRequisiteCourses,
 } from './course.interface';
 
-const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: 'Course',
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isDeleted: {
-    type: Boolean,
-    default: false,
+  {
+    _id: false,
   },
-});
+);
 
-const courseSchema = new Schema<TCourse>({
-  title: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: true,
+const courseSchema = new Schema<TCourse>(
+  {
+    title: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    prefix: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    code: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  prefix: {
-    type: String,
-    trim: true,
-    required: true,
+  {
+    timestamps: true,
   },
-  code: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  credits: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 export const Course = model<TCourse>('Course', courseSchema);
 
